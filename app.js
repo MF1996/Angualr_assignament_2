6386fa022a66510eca0523d316bb87407ac9cb1e
feim <igmo199@gmail.com>
2018-10-17 20:53:04 +0200

    Defining list of object ToBuy

--- app.js
+++ app.js
@@ -8,6 +8,28 @@
 ToBuyController.$inject =['ShoppingListCheckOffService']
 function ToBuyController(ShoppingListCheckOffService) {
    buy = this
+   buy.ToBuy = [
+        {
+          name: "cookies",
+           quantity: 10
+         }
+        ,{
+          name: "Milk",
+           quantity: 5
+         },
+        {
+          name: "chips",
+           quantity: 3
+         },
+        {
+          name: "Juice",
+           quantity: 1
+         },
+        {
+          name: "Eggs",
+           quantity: 10
+         }
+   ]
 
 }
 
